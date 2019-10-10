1e5b66d25acc5f318b2cf8aee1a90f5568703182
Axell B <56314049+AxellBoue@users.noreply.github.com>
2019-10-10 15:55:06 +0200

    lauterc boit et pause

--- scripts/interactions/table.gd
+++ scripts/interactions/table.gd
@@ -7,28 +7,36 @@ var cameraBox
 var assis = false
 onready var sonChaise = preload("res://sons/chaise v2.wav")
 var debut = true
+onready var lautrecAssis = get_node("lautrecAssis")
 
 # voix debut
 onready var voixdebut = preload("res://sons/Voix Off Intro.wav")
 var debutVoix = true
+
+# boit
+onready var timer = get_node("Timer")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	lautrec = get_node("/root/Node2D/Lautrec")
 	sm = get_node("/root/Node2D/gameManager/soundManager")
 	cameraBox = get_node("/root/Node2D/gameManager/cameraBox")
+	timer.connect("timeout",self,"boit")
 	interaction()
 
 func interaction():
-	get_node("lautrecAssis").visible = !assis
+	lautrecAssis.visible = !assis
 	get_node("table sans lautrec").visible = assis
 	lautrec.set_visible(assis)
 	lautrec.bloque(!assis)
 	if assis:
 		cameraBox.change_zoom(1.2,8)
+		timer.stop()
 	else :
 		cameraBox.change_zoom(0.7,8)
 		get_node("objInteractif").feedback.visible = false
+		lautrecAssis.play("assis")
+		timer.start()
 	if !debut:
 		sm.play_new_son(get_node("AudioStreamPlayer2D"), sonChaise)
 	else : 
@@ -40,5 +48,9 @@ func interaction():
 		nm.lance_texte(nm.intro)
 	assis = !assis
 
-func anim_texte_1(var num):
-	pass
+func boit():
+	lautrecAssis.play("boit")
+	
+func fin_anim():
+	if lautrecAssis.animation == "boit":
+		lautrecAssis.play("assis")
